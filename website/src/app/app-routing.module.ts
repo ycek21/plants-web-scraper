229b85509cf3a2ba3759b03193c6755aea386fe7
Jacek Eichler <39658861+ycek21@users.noreply.github.com>
2020-08-25 22:50:10 +0200

    Project structure. HomePage done without photos

--- website/src/app/app-routing.module.ts
+++ website/src/app/app-routing.module.ts
@@ -1,11 +1,19 @@
+import { PlantComponent } from './modules/plant/plant.component';
+import { HomePageComponent } from './modules/home-page/home-page.component';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: HomePageComponent},
+  { path: 'home', component: HomePageComponent},
+  { path: 'plant/:plantType', component: PlantComponent},
+  { path: '**', component: HomePageComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
+export const routingComponents = [HomePageComponent, PlantComponent];
