3e8bf89f13ebdffa326cbb41abaab00dc0d77263
Jacek Eichler <39658861+ycek21@users.noreply.github.com>
2020-08-28 22:41:17 +0200

    Photos appear with different border color.

--- website/src/app/modules/plant-card/plant-card.component.ts
+++ website/src/app/modules/plant-card/plant-card.component.ts
@@ -7,6 +7,7 @@ import { Component, OnInit, Input } from "@angular/core";
 })
 export class PlantCardComponent implements OnInit {
   @Input() plantPhoto;
+  @Input() photoCardIndex;
 
   constructor() {}
 
