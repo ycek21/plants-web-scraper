5e0e5a13056bb89338046ec6e51562a12f6065ca
Stanisław Kucznerowicz <46819190+stasiu9@users.noreply.github.com>
2020-08-30 21:45:07 +0200

    Added pexels service

--- website/src/app/modules/plant-list/plant-list.component.ts
+++ website/src/app/modules/plant-list/plant-list.component.ts
@@ -1,36 +1,48 @@
-import { PlantsService } from './../../core/services/plants.service';
-import { Component, OnInit } from '@angular/core';
+import { PexelsService } from "./../../core/services/pexels.service";
+import { PlantsService } from "./../../core/services/plants.service";
+import { Component, OnInit } from "@angular/core";
+import { report } from "process";
 
 @Component({
-  selector: 'app-plant-list',
-  templateUrl: './plant-list.component.html',
-  styleUrls: ['./plant-list.component.css'],
+  selector: "app-plant-list",
+  templateUrl: "./plant-list.component.html",
+  styleUrls: ["./plant-list.component.css"],
 })
 export class PlantListComponent implements OnInit {
   plantsType: string[] = [
-    'rose',
-    'tulip',
-    'orchid',
-    'cactus',
-    'sunflower',
-    'hydrangea',
-    'fern',
-    'lavender',
-    'palm'
+    "rose",
+    "tulip",
+    "orchid",
+    "cactus",
+    "sunflower",
+    "hydrangea",
+    "fern",
+    "lavender",
+    "palm",
   ];
-  selectedPlant = 'cactus';
+  selectedPlant = "cactus";
   plantsURL: string[] = [];
   slideToggle = true;
-
-  constructor(private plantsService: PlantsService) {}
+  plantPexelsURL: string[] = [];
+  constructor(
+    private plantsService: PlantsService,
+    private pexelsService: PexelsService
+  ) {}
 
   ngOnInit(): void {
     this.getPlantsURLs(this.selectedPlant);
+    this.pexelsService.getPhotos("rose", 10).subscribe((resp) => {
+      console.log(resp.photos[0].src.original);
+      for (var i = 0; i < resp.photos.length; i++) {
+        this.plantPexelsURL.push(resp.photos[i].src.original);
+      }
+      console.log(this.plantPexelsURL);
+    });
   }
 
   getPlantsURLs(plantType: string) {
-    this.plantsService.getPhotoList(plantType).subscribe( response => {
-      this.plantsURL = response['scrapedPhotosLinks'];
+    this.plantsService.getPhotoList(plantType).subscribe((response) => {
+      this.plantsURL = response["scrapedPhotosLinks"];
     });
   }
 
--- website/src/app/shared/consts.ts
+++ website/src/app/shared/consts.ts
@@ -1,3 +1,6 @@
-export const URL = 'http://localhost:8085/api/';
+export const URL = "http://localhost:8085/api/";
 
-export const PHOTOS_URL = URL + 'plant-photos/scrape?plantType=:/TYPE:/';
+export const PHOTOS_URL = URL + "plant-photos/scrape?plantType=:/TYPE:/";
+
+export const API_KEY =
+  "563492ad6f9170000100000170e55d761239421498bc16f4ee1bc617";
