d5f08c5274a2d7cdfabf51cb35f33eff4555f0dc
Jacek Eichler <39658861+ycek21@users.noreply.github.com>
2020-08-31 20:13:52 +0200

    PlantPage description - not finished owing to the wrong JSON from API

--- website/src/app/modules/plant-page/plant-page.component.ts
+++ website/src/app/modules/plant-page/plant-page.component.ts
@@ -1,32 +1,51 @@
-import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute, Router } from '@angular/router';
+import { WikipediaDataService } from "./../../core/services/wikipedia-data.service";
+import { Component, OnInit } from "@angular/core";
+import { ActivatedRoute, Router } from "@angular/router";
 
 @Component({
-  selector: 'app-plant-page',
-  templateUrl: './plant-page.component.html',
-  styleUrls: ['./plant-page.component.css']
+  selector: "app-plant-page",
+  templateUrl: "./plant-page.component.html",
+  styleUrls: ["./plant-page.component.css"],
 })
 export class PlantPageComponent implements OnInit {
-  plantType = '';
-  plantPhotoUrl = 'http://localhost:8085/api/photos/';
+  plantType = "";
+  plantPhotoUrl = "http://localhost:8085/api/photos/";
+  plantDescription = "";
+  plantKingdom = "";
+  plantOrder = "";
+  plantFamily = "";
 
-  constructor(private route: ActivatedRoute,
-    private router: Router) { }
+  constructor(
+    private route: ActivatedRoute,
+    private router: Router,
+    private wikipediaService: WikipediaDataService
+  ) {}
 
   ngOnInit(): void {
     this.preparePage();
   }
 
   preparePage() {
-    this.plantType = this.route.snapshot.paramMap.get('plantType').toUpperCase();
+    this.plantType = this.route.snapshot.paramMap
+      .get('plantType')
+      .toUpperCase();
     this.plantPhotoUrl += this.plantType.toLowerCase() + '/';
-    this.plantPhotoUrl += this.route.snapshot.paramMap.get('id');;
+    this.plantPhotoUrl += this.route.snapshot.paramMap.get('id');
 
+    this.getPlantInfo();
   }
 
   navigateToHome() {
-    this.router.navigate(['/home']);
+    this.router.navigate(["/home"]);
   }
 
+  getPlantInfo() {
+    this.wikipediaService
+      .getPlantData(this.plantType.toLowerCase())
+      .subscribe((response) => {
+        // let sth = response.toString().replace('\\', '');
+        // // let sth2 = JSON.parse(sth);
+        // console.log('DATA:', sth)
+      });
+  }
 }
-
--- website/src/app/shared/consts.ts
+++ website/src/app/shared/consts.ts
@@ -1,3 +1,5 @@
 export const URL = 'http://localhost:8085/api/';
 
 export const PHOTOS_URL = URL + 'plant-photos/scrape?plantType=:/TYPE:/';
+
+export const DATA_API_URL = 'http://localhost:5000/info/:/TYPE:/';
