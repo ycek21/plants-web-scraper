96f2ef2b1b1d7f7cb24e7137446a3451c1750cd6
Jacek Eichler <39658861+ycek21@users.noreply.github.com>
2020-09-06 17:55:15 +0200

    Display spinner while loading data

--- website/src/app/modules/plant-list/plant-list.component.ts
+++ website/src/app/modules/plant-list/plant-list.component.ts
@@ -1,32 +1,32 @@
-import { PexelsService } from './../../core/services/pexels.service';
-import { PlantsService } from './../../core/services/plants.service';
-import { Component, OnInit, Input } from '@angular/core';
+import { PexelsService } from "./../../core/services/pexels.service";
+import { PlantsService } from "./../../core/services/plants.service";
+import { Component, OnInit, Input } from "@angular/core";
 
 @Component({
-  selector: 'app-plant-list',
-  templateUrl: './plant-list.component.html',
-  styleUrls: ['./plant-list.component.css'],
+  selector: "app-plant-list",
+  templateUrl: "./plant-list.component.html",
+  styleUrls: ["./plant-list.component.css"],
 })
 export class PlantListComponent implements OnInit {
   plantsType: string[] = [
-    'rose',
-    'tulip',
-    'orchid',
-    'cactus',
-    'sunflower',
-    'hydrangea',
-    'fern',
-    'lavender',
-    'palm',
+    "rose",
+    "tulip",
+    "orchid",
+    "cactus",
+    "sunflower",
+    "hydrangea",
+    "fern",
+    "lavender",
+    "palm",
   ];
-  selectedPlant = '';
+  selectedPlant = "";
   plantsURL: string[] = [];
   slideToggle = true;
+  loadingPhotosList;
   @Input() forPlantPage = false;
-  @Input() plantType = '';
+  @Input() plantType = "";
   @Input() displayedInPexelsPage = false;
 
-  // plantPexelsURL: string[] = [];
   constructor(
     private plantsService: PlantsService,
     private pexelsService: PexelsService
@@ -41,7 +41,8 @@ export class PlantListComponent implements OnInit {
     if (this.slideToggle) {
       if (!this.displayedInPexelsPage) {
         this.plantsService.getPhotoList(plantType).subscribe((response) => {
-          this.plantsURL = response['scrapedPhotosLinks'];
+          this.plantsURL = response["scrapedPhotosLinks"];
+          this.loadingPhotosList = false;
         });
       } else {
         this.pexelsService
@@ -51,34 +52,44 @@ export class PlantListComponent implements OnInit {
             for (var i = 0; i < resp.photos.length; i++) {
               this.plantsURL.push(resp.photos[i].src.original);
             }
+
+            this.loadingPhotosList = false;
           });
       }
     } else {
-      this.pexelsService.getPhotos(this.selectedPlant, 500).subscribe((resp) => {
-        this.plantsURL = [];
-        for (var i = 0; i < resp.photos.length; i++) {
-          this.plantsURL.push(resp.photos[i].src.original);
-        }
-      });
+      this.pexelsService
+        .getPhotos(this.selectedPlant, 500)
+        .subscribe((resp) => {
+          this.plantsURL = [];
+          for (var i = 0; i < resp.photos.length; i++) {
+            this.plantsURL.push(resp.photos[i].src.original);
+          }
+
+          this.loadingPhotosList = false;
+        });
     }
   }
 
   selectPlant(plant: string) {
+    this.loadingPhotosList = true;
     this.selectedPlant = plant;
     this.getPlantsURLs(this.selectedPlant);
   }
 
   slideToggler() {
+    this.loadingPhotosList = true;
     this.slideToggle = !this.slideToggle;
   }
 
   changePhotoSource() {
+    this.loadingPhotosList = true;
     this.getPlantsURLs(this.selectedPlant);
   }
 
   selectPlantAtTheBeginning() {
-    if ( this.plantType === '' ) {
-      this.selectedPlant = 'cactus';
+    this.loadingPhotosList = true;
+    if (this.plantType === "") {
+      this.selectedPlant = "cactus";
     } else {
       this.selectedPlant = this.plantType;
     }
--- website/src/app/shared/material/material.module.ts
+++ website/src/app/shared/material/material.module.ts
@@ -3,10 +3,12 @@ import { CommonModule } from "@angular/common";
 import { MatIconModule } from "@angular/material/icon";
 import { MatChipsModule } from "@angular/material/chips";
 import { MatSlideToggleModule } from "@angular/material/slide-toggle";
+import {MatProgressSpinnerModule} from '@angular/material/progress-spinner';
+
 
 @NgModule({
   declarations: [],
-  imports: [CommonModule, MatIconModule, MatChipsModule, MatSlideToggleModule],
-  exports: [MatIconModule, MatChipsModule, MatSlideToggleModule],
+  imports: [CommonModule, MatIconModule, MatChipsModule, MatSlideToggleModule, MatProgressSpinnerModule],
+  exports: [MatIconModule, MatChipsModule, MatSlideToggleModule, MatProgressSpinnerModule],
 })
 export class MaterialModule {}
